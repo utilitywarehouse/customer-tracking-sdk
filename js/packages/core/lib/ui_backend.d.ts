17be23f49ff38adb292fdf7e37d02048627f813f
Michael Donat <michael.donat@me.com>
2020-07-23 00:38:39 +0100

    cleasnup

--- js/packages/core/lib/ui_backend.d.ts
+++
@@ -1,8 +0,0 @@
-import { Backend } from "./backend";
-import { Account } from "@utilitywarehouse/customer-tracking-types";
-export interface UIBackend extends Backend {
-    enable(): Promise<void>;
-    disable(): Promise<void>;
-    identify(account: Account): Promise<void>;
-    reset(): Promise<void>;
-}
